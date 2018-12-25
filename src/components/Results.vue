753e68b64f50b215f190f1c25e90cf4b1b1df3e6
santiagoaldanondo <santiagoaldanondo@gmail.com>
2018-12-25 23:22:43 +0100

    feat(): add components, router and store

---
+++ src/components/Results.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <v-layout column>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        <h1>Results</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
