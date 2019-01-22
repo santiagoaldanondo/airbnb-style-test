a2d0c7c55d28197b7e288c8dd4ba40568b938676
santiagoaldanondo <santiagoaldanondo@gmail.com>
2019-01-23 00:26:18 +0100

    fix(): fix link from Landing to Test

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -6,16 +6,16 @@
       </v-flex>
       <v-flex xs12 sm6 offset-sm3 mt-3>
         <blockquote class="blockquote text-xs-center">
-          test your knowledge!
+          Test your knowledge!
         </blockquote>
       </v-flex>
       <v-flex xs12 sm6 offset-sm3 class="text-xs-center" mt-5>
-        <v-btn color="primary" to="/home">Get started</v-btn>
+        <v-btn color="primary" to="/test">Get started</v-btn>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {}
+  export default {}
 </script>
